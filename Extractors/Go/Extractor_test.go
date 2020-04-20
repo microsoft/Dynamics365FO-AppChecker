1be9331d563975613f51f475c1b9dd8a554a92a5
Peter Villadsen <pvillads@users.noreply.github.com>
2020-04-19 23:25:40 -0700

    Removed reference to unused variable

--- Extractors/Go/Extractor_test.go
+++ Extractors/Go/Extractor_test.go
@@ -39,28 +39,6 @@ func createExpressionDocument(snippet string, t *testing.T) *etree.Document {
 	return doc
 }
 
-func TestComment(t *testing.T) {
-	doc := createFileDocument(`
-// This is the package comment.
-package main
-
-// This comment is associated with the hello constant.
-const hello = "Hello, World!" // line comment 1
-
-// This comment is associated with the foo variable.
-var foo = hello // line comment 2
-
-// This comment is associated with the main function.
-func main() {
-	fmt.Println(hello) // line comment 3
-}`, t)
-
-	doc.Indent(2)
-	s, _ := doc.WriteToString()
-	// fmt.Print(s)
-
-	// TODO
-}
 func TestCommentGroup(t *testing.T) {
 
 }
